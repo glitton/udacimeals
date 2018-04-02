bc895d3316927426902c1f37b9838b51a5118bbf
Generosa Litton <glcodeworks@gmail.com>
2018-04-01 17:05:13 -0700

    practice redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,20 @@
-export const ADD_RECIPE = 'ADD_RECIPE';
-export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR';
+export const ADD_RECIPE = "ADD_RECIPE";
+export const REMOVE_FROM_CALENDAR = "REMOVE_FROM_CALENDAR";
 
 //action creators
-export function addRecipe ({ day, recipe, meal }) {
+export function addRecipe({ day, recipe, meal }) {
   return {
     type: ADD_RECIPE,
     recipe,
     day,
-    meal,
-  }
+    meal
+  };
 }
 
-export function removeFromCalendar ({ day, meal }) {
-  type: REMOVE_FROM_CALENDAR,
-  day,
-  meal
+export function removeFromCalendar({ day, meal }) {
+  return {
+    type: REMOVE_FROM_CALENDAR,
+    day,
+    meal
+  };
 }
